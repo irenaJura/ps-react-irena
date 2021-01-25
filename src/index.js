12a34829ac43f57d1cb913c597c4d575539d1a8e
Jurasek Irena <jurasek@eurisnet.it>
2021-01-25 14:08:20 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Docs from './docs/Docs';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import '../node_modules/highlight.js/styles/ocean.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Docs />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
